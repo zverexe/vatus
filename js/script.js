37dbbca8dc24e0c54a4180754c35c497a866aace
deadly8 <tarasent@ukr.net>
2017-03-31 21:07:33 +0300

    added new pages

--- js/script.js
+++ js/script.js
@@ -14,25 +14,67 @@ var $menu = $("#mobile-menu").mmenu({
          ],
          }
          ]*/
+        extensions: {
+            "(max-width: 550px)": ["fullscreen"]
+        },
+
         "navbar": {
             title: ""
         },
         "navbars": [
             {
+                "height"  : 1,
                 "position": "top",
                 "content": [
                     "searchfield"
+                ]
+            },
+            {
+
+                "position": "top",
+                "content": ["<img src='img/close-menu.png' id='close-menu' class='close-icon'>",
+                    'close'
                 ]
             }
         ]
     },
     {
+
         "searchfield": {
             "clear": true
         }
+
     });
 var $icon = $("#header__button");
 var API = $menu.data( "mmenu" );
 $icon.on( "click", function() {
     API.open();
+});
+
+var $iconClose = $("#close-menu");
+$iconClose.on( "click", function() {
+    API.close();
+});
+
+
+
+
+$('[data-toggle="collapse"]').click(function() {
+    $('.collapse.in').collapse('hide')
+});
+
+
+$('.collapse').on('show.bs.collapse', function(){
+    var i = $(this).parent().find('.fa-chevron-down');
+    i.toggleClass('fa-chevron-down fa-chevron-up');
+
+}).on('hide.bs.collapse', function(){
+    var i = $(this).parent().find('.fa-chevron-up');
+    i.toggleClass('fa-chevron-up fa-chevron-down');
+});
+
+
+$('.left-menu-link').on('click', function () {
+    var a = $(this).parent().find('.menu-link');
+    a.toggleClass('menu-link-opened');
 });

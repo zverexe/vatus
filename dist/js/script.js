5baba76ffa33713b11b49d4934c6f095eac9fe5f
deadly8 <tarasent@ukr.net>
2017-06-23 11:46:36 +0300

    changed path's to files, refactored js code

--- dist/js/script.js
+++ dist/js/script.js
@@ -72,64 +72,29 @@ $( document ).ready(function() {
         i.toggleClass('fa-chevron-up fa-chevron-down');
     });
 
-
-    /*
-     * check all checkboxes
-     */
-    /*$("#check-all").on('click', function(){
-        if ($(this).hasClass('allChecked')) {
-            $('input[type="checkbox"]', '.todo-list').prop('checked', false);
-        } else {
-            $('input[type="checkbox"]', '.todo-list').prop('checked', true);
-        }
-        $(this).toggleClass('allChecked');
-    });*/
-
-    /*$(".check-all").on('click', function(){
-        if ($(this).hasClass('allChecked')) {
-            $('input[type="checkbox"]', '.todo-list-3').prop('checked', false);
-        } else {
-            $('input[type="checkbox"]', '.todo-list-3').prop('checked', true);
-        }
-        $(this).toggleClass('allChecked');
-    });*/
-
-
-
-    function modifyText(e) {
-        console.log(e);
-        var listElements = Array.prototype.slice.call(e.path[2].children[1].children);
-        var checked= listElements.every(item => item.children[0].children[0].checked);
-        listElements.forEach((item)=>{
-            let t = item.children["0"].children["0"];
-            t.checked = !checked;
-        });
-    }
-
-    var el = document.getElementById("check-all-2");
-    el.addEventListener("click", modifyText, false);
-
-
-
-    /*$("#check-all-1").on('click', function(){
-        if ($(this).hasClass('allChecked')) {
-            $('input[type="checkbox"]', '.todo-list').prop('checked', false);
-        } else {
-            $('input[type="checkbox"]', '.todo-list-2').prop('checked', true);
-        }
-        $(this).toggleClass('allChecked');
-    });*/
-
-
-
     /*
      * add note
      */
 
-    $('.add-note').click(function() {
+    $('.add-note').click(function(e) {
+        console.log(e);
         $(this).prev().find('.note').toggle('slow', function() {
             // Animation complete.
+
         });
     });
-
 });
+
+/*
+ * check all checkboxes
+ */
+
+function selectAll(event) {
+    console.log(event);
+    var listElements = Array.prototype.slice.call(event.path[2].children[1].children);
+    var checked= listElements.every(item => item.children[0].children[0].checked);
+    listElements.forEach((item)=>{
+        var t = item.children["0"].children["0"];
+    t.checked = !checked;
+});
+}

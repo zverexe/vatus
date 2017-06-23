fe21cf80354dac7bd80c4258905a801dbaa6ab00
deadly8 <tarasent@ukr.net>
2017-06-23 13:35:39 +0300

    changed path's to files

--- js/script.js
+++ js/script.js
@@ -1,143 +1,100 @@
-/*
- * Mobile menu
- */
-var $menu = $("#mobile-menu").mmenu({
-        extensions: {
-            "(max-width: 550px)": ["fullscreen"]
-        },
-
-        "navbar": {
-            title: ""
-        },
-        "navbars": [
-            {
-                "height"  : 1,
-                "position": "top",
-                "content": [
-                    "searchfield"
-                ]
+$( document ).ready(function() {
+    /*
+     * Mobile menu
+     */
+    var $menu = $("#mobile-menu").mmenu({
+            extensions: {
+                "(max-width: 550px)": ["fullscreen"]
             },
-            {
-
-                "position": "top",
-                "content": ["<img src='img/close-menu.png' id='close-menu' class='close-icon'>",
-                    'close'
-                ]
+            "navbar": {
+                title: ""
+            },
+            "navbars": [
+                {
+                    "height"  : 1,
+                    "position": "top",
+                    "content": [
+                        "searchfield"
+                    ]
+                },
+                {
+                    "position": "top",
+                    "content": ["<img src='img/close-menu.png' id='close-menu' class='close-icon'>",
+                        'close'
+                    ]
+                }
+            ]
+        },
+        {
+            "searchfield": {
+                "clear": true
             }
-        ]
-    },
-    {
+        });
 
-        "searchfield": {
-            "clear": true
-        }
-
+    var $icon = $("#header__button");
+    var API = $menu.data( "mmenu" );
+    $icon.on( "click", function() {
+        API.open();
     });
-var $icon = $("#header__button");
-var API = $menu.data( "mmenu" );
-$icon.on( "click", function() {
-    API.open();
-});
 
-var $iconClose = $("#close-menu");
-$iconClose.on( "click", function() {
-    API.close();
-});
-
-$(window).on("resize", function() {
-    if (window.matchMedia("(min-width: 992px)").matches) {
+    var $iconClose = $("#close-menu");
+    $iconClose.on( "click", function() {
         API.close();
-    }
-});
+    });
 
-/*
- * end mobile mmenu
- */
-
-
-/*
- * Open only one submenu
- */
-
-$('[data-toggle="collapse"]').click(function() {
-    $('.collapse.in').collapse('hide')
-});
-
-/*
- *
- */
-
-
-/*
- * Change left menu chevrons on collapse submenu
- */
-
-$('.collapse').on('show.bs.collapse', function(){
-    var i = $(this).parent().find('.fa-chevron-down');
-    i.toggleClass('fa-chevron-down fa-chevron-up');
-
-}).on('hide.bs.collapse', function(){
-    var i = $(this).parent().find('.fa-chevron-up');
-    i.toggleClass('fa-chevron-up fa-chevron-down');
-});
-/*
- *
- */
+    $(window).on("resize", function() {
+        if (window.matchMedia("(min-width: 992px)").matches) {
+            API.close();
+        }
+    });
 
 
 
-/*
-* check all checkboxes
- */
-$("#check-all").on('click', function(){
-    if ($(this).hasClass('allChecked')) {
-        $('input[type="checkbox"]', '.todo-list').prop('checked', false);
-    } else {
-        $('input[type="checkbox"]', '.todo-list').prop('checked', true);
-    }
-    $(this).toggleClass('allChecked');
- });
+    /*
+     * Open only one submenu
+     */
 
-$("#check-all-2").on('click', function(){
-    if ($(this).hasClass('allChecked')) {
-        $('input[type="checkbox"]', '.todo-list-3').prop('checked', false);
-    } else {
-        $('input[type="checkbox"]', '.todo-list-3').prop('checked', true);
-    }
-    $(this).toggleClass('allChecked');
-});
+    $('[data-toggle="collapse"]').click(function() {
+        $('.collapse.in').collapse('hide')
+    });
 
 
-$("#check-all-1").on('click', function(){
-    if ($(this).hasClass('allChecked')) {
-        $('input[type="checkbox"]', '.todo-list').prop('checked', false);
-    } else {
-        $('input[type="checkbox"]', '.todo-list-2').prop('checked', true);
-    }
-    $(this).toggleClass('allChecked');
-});
+    /*
+     * Change left menu chevrons on collapse submenu
+     */
 
+    $('.collapse').on('show.bs.collapse', function(){
+        var i = $(this).parent().find('.fa-chevron-down');
+        i.toggleClass('fa-chevron-down fa-chevron-up');
 
+    }).on('hide.bs.collapse', function(){
+        var i = $(this).parent().find('.fa-chevron-up');
+        i.toggleClass('fa-chevron-up fa-chevron-down');
+    });
 
-/*
-* add note
- */
+    /*
+     * add note
+     */
 
+    $('.add-note').click(function(e) {
+        console.log(e);
+        $(this).prev().find('.note').toggle('slow', function() {
+            // Animation complete.
 
-$('#add-note').click(function() {
-    $('.add-note').toggle('slow', function() {
-        // Animation complete.
+        });
     });
 });
 
-$('#add-note-1').click(function() {
-    $('.add-note-1').toggle('slow', function() {
-        // Animation complete.
-    });
-});
+/*
+ * check all checkboxes
+ */
 
-$('#add-note-2').click(function() {
-    $('.add-note-2').toggle('slow', function() {
-        // Animation complete.
-    });
+function selectAll(event) {
+    console.log(event);
+    var listElements = Array.prototype.slice.call(event.path[2].children[1].children);
+    var checked= listElements.every(item => item.children[0].children[0].checked);
+    listElements.forEach((item)=>{
+        var t = item.children["0"].children["0"];
+    t.checked = !checked;
 });
+}
